5ca949a17d694f97519a0d5a5bb033cb2347a252
sabarim6369 <sabarim6369@gmail.com>
2025-04-07 23:08:55 +0530

    added changes

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 // api.js
 // const API_URL="http://192.168.19.24:5000/api"
-const API_URL="https://thoughts-backend-yoby.onrender.com/api"
+// const API_URL="https://thoughts-backend-yoby.onrender.com/api"
+const API_URL="http://51.20.227.27/api"
 export default API_URL;
 // https://thoughts-backend-yoby.onrender.comr
